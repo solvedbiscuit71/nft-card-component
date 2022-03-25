cf5e2f794a8fce6069922f274f90497663df6805
Praveen Perumal <ppraveen98841@gmail.com>
2022-03-25 07:58:27 +0530

    Resolve HTML Validation issue
    Error:
    Attribute `color` not allowed on element `div` at this point.
    
    Context:
    ```
    -1gozfj0`><div color=`hsl(178, 100%, 50%)` class=`css-1af3pso`><svg w
    ```

--- src/App.jsx
+++ src/App.jsx
@@ -40,11 +40,11 @@ function App() {
         </CardContent>
 
         <BadgeWrapper>
-          <Badge color="hsl(178, 100%, 50%)">
+          <Badge bold>
             <svg width="11" height="18" xmlns="http://www.w3.org/2000/svg"><path d="M11 10.216 5.5 18 0 10.216l5.5 3.263 5.5-3.262ZM5.5 0l5.496 9.169L5.5 12.43 0 9.17 5.5 0Z" fill="#00FFF8"/></svg>
             0.041 ETH
           </Badge>
-          <Badge color="hsl(215, 51%, 70%)">
+          <Badge>
             <svg width="17" height="17" xmlns="http://www.w3.org/2000/svg"><path d="M8.305 2.007a6.667 6.667 0 1 0 0 13.334 6.667 6.667 0 0 0 0-13.334Zm2.667 7.334H8.305a.667.667 0 0 1-.667-.667V6.007a.667.667 0 0 1 1.334 0v2h2a.667.667 0 0 1 0 1.334Z" fill="#8BACD9"/></svg>
             3 days left
           </Badge>
--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export default styled.div(props => (
   {
     alignItems: "center",
-    color: props.color,
+    color: props.bold ? "hsl(178, 100%, 50%)" : "hsl(215, 51%, 70%)",
     display: "flex",
     fontWeight: "500",
     gap: 5
